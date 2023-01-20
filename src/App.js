f400ea64bf7734f90a5337595c962ff39186e4b9
weston-bailey <wdbailey87@gmail.com>
2023-01-19 17:02:23 -0800

    adds solve

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+
+import Houses from './Houses'
+import House from './House'
+import Member from './Member'
+
+import gameOfThrones from './gameOfThrones'
+
 export default function App() {
+  console.log(gameOfThrones)
   return (
-    <div></div>
+    <Router>
+      <Routes>
+        <Route 
+          element={<Houses gameOfThrones={gameOfThrones} />}
+          path='/'
+        />
+
+        <Route 
+          path='/houses/:id'
+          element={<House gameOfThrones={gameOfThrones } />}
+        />
+
+        <Route 
+          path='/houses/:houseId/members/:memberId'
+          element={<Member gameOfThrones={gameOfThrones } />}
+        />
+      </Routes>
+    </Router>
   )
 }
